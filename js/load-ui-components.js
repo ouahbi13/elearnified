8b9068da74d31ed24d00c6d79c40c8ea91468586
Aymane <aymane.ouahbi@gmail.com>
2021-12-24 22:54:59 +0100

    first commit

---
+++ js/load-ui-components.js
@@ -0,0 +1,5 @@
+$(".nav-container").load("./components/navbar.html")
+$(".footer-container").load("./components/footer.html");
+$(".faq-container").load("./components/faq.html");
+$(".update-container").load("./components/update.html");
+$(".browse-container").load("./components/browse.html");
